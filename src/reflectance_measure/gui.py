ec150c46037779407e9837901065407943edbd9e
Michael Biselx <mw.biselx@bluewin.ch>
2023-07-03 16:53:15 +0200

    update GUI

--- src/reflectance_measure/gui.py
+++ src/reflectance_measure/gui.py
@@ -9,7 +9,8 @@ else:
     from pyqtgraph.Qt.QtCore import Qt, QTimer
     from pyqtgraph.Qt.QtWidgets import *
 
-from reflectance_measure.stage.stage_gui import StageSelector, StageControl
+from reflectance_measure.stage.stage_gui import StageSelector, StageMonitor, StageControl
+from reflectance_measure.daq.daq_gui import DAQSelector, DAQMonitor
 
 
 class DynamicLayoutDockWidget(QDockWidget):
@@ -32,35 +33,51 @@ class MyMainWindow(QMainWindow):
     def __init__(self, parent: QWidget | None = None) -> None:
         super().__init__(parent)
 
+        # scatterplot as central widget
         self._scatter = pg.ScatterPlotItem()
-
         self._plot = pg.PlotWidget(self)
         self._plot.addItem(self._scatter)
-
-        self._stage_selector = StageSelector(self)
-        self._stage_selector_dock = DynamicLayoutDockWidget(
-            "stage selector", self)
-        self._stage_selector_dock.setWidget(self._stage_selector)
-
-        self._stage_control = StageControl(self, self._stage_selector.stage)
-        self._stage_control_dock = DynamicLayoutDockWidget(
-            "stage control", self)
-        self._stage_control_dock.setWidget(self._stage_control)
-
-        self._stage_control_dock.dockLocationChanged.connect(
-            self._stage_control_dock.update_layout)
-
         self.setCentralWidget(self._plot)
 
         # add stage control widgets on the left
+        self._stage_info = StageMonitor(self)
+        self._stage_info_dock = QDockWidget(
+            "stage info", self)
+        self._stage_info_dock.setWidget(self._stage_info)
+        self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea,
+                           self._stage_info_dock)
+
+        self._stage_selector = StageSelector(self, self._stage_info.stage)
+        self._stage_selector_dock = QDockWidget(
+            "stage selector", self)
+        self._stage_selector_dock.setWidget(self._stage_selector)
         self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea,
                            self._stage_selector_dock)
+
+        self._stage_control = StageControl(self, self._stage_info.stage)
+        self._stage_control_dock = QDockWidget(
+            "stage control", self)
+        self._stage_control_dock.setWidget(self._stage_control)
         self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea,
                            self._stage_control_dock)
+
         self.tabifyDockWidget(self._stage_control_dock,
                               self._stage_selector_dock)
 
         # add photodiode control widgets on the right
+        self._daq_info = DAQMonitor(self)
+        self._daq_info_dock = QDockWidget(
+            "daq info", self)
+        self._daq_info_dock.setWidget(self._daq_info)
+        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea,
+                           self._daq_info_dock)
+
+        self._daq_selector = DAQSelector(self, self._daq_info.daq)
+        self._daq_selector_dock = QDockWidget(
+            "daq selector", self)
+        self._daq_selector_dock.setWidget(self._daq_selector)
+        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea,
+                           self._daq_selector_dock)
 
 
 if __name__ == "__main__":
